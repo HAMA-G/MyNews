4ba2542e32a2991371bb5ed34457b7ae3dc252b5
KOKI <k-hamaji2525@gmail.com>
2022-02-09 15:02:10 +0000

    Laravel11 problem end

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Myprofile</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'My プロフィール'を埋め込む --}}
+@section('title', 'My プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div clas="row">
+            <h2>My プロフィール</h2>
+        </div>
+    </div>
+@endsection
