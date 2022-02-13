4ac684d8194a3c13f71f2e52aa81a982b4d8c4ee
KOKI <k-hamaji2525@gmail.com>
2022-02-13 00:24:43 +0000

    Laravel09 problem4 resubmit

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,9 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-});
-
-Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
-});
-
-Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
-
 
 Auth::routes();
 
