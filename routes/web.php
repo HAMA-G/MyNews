d24106a1488b30e5f9a0e1941530df55da95fcb9
KOKI <k-hamaji2525@gmail.com>
2022-02-23 14:55:37 +0000

    Laravel16 problem

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
-    Route::post('profie/edit', 'Admin\ProfileController@update'); 
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profie/edit', 'Admin\ProfileController@update')->middleware('auth'); 
 });
 
 Auth::routes();
